04a9af6816fc60011811e008aaa17ba575302f34
HrfxH <163137286+HrfxH@users.noreply.github.com>
2026-09-09 23:49:59 +0000

    feat: cria estrutura inicial da calculadora

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+print("=== CALCULADORA DE PONTUAÇÃO ===")
+
+presenca = float(input("Digite a pontuação de Presença: "))
+posicionamento = float(input("Digite a pontuação de Posicionamento: "))
+ponto_extra = float(input("Digite a pontuação de Ponto Extra: "))
+
+print("Pontuações cadastradas com sucesso!")
