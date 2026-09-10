4e748adc475f3f92faa5a661e7f7731f40d3b184
HrfxH <163137286+HrfxH@users.noreply.github.com>
2026-09-10 00:02:30 +0000

    feat: adiciona validação das pontuacoes

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,14 @@
 print("=== CALCULADORA DE PONTUAÇÃO ===")
 
-presenca = float(input("Digite a pontuação de Presença: "))
-posicionamento = float(input("Digite a pontuação de Posicionamento: "))
-ponto_extra = float(input("Digite a pontuação de Ponto Extra: "))
+presenca = float(input("Digite a pontuação de Presença (0 a 100): "))
+posicionamento = float(input("Digite a pontuação de Posicionamento (0 a 100): "))
+ponto_extra = float(input("Digite a pontuação de Ponto Extra (0 a 100): "))
 
-print("Pontuações cadastradas com sucesso!")
+if 0 <= presenca <= 100 and 0 <= posicionamento <= 100 and 0 <= ponto_extra <= 100:
+    print("Pontuações cadastradas com sucesso!")
 
-media = (presenca + posicionamento + ponto_extra) /3
+    media = (presenca + posicionamento + ponto_extra) / 3
 
-print(f"media final:{media:.2f}")
+    print(f"Média final: {media:.2f}")
+else:
+    print("Erro: todas as pontuações devem estar entre 0 e 100.")
